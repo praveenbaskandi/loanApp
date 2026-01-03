a1d64be594c755ad40a833cbc15122153c48bdc5
Praveen Baskandi <praveenbaskandi@Praveens-MacBook-Air.local>
2026-01-03 20:03:03 +0530

    github CLI added for makeing build

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,13 +1,22 @@
-/**
- * @format
- */
-
 import React from 'react';
-import ReactTestRenderer from 'react-test-renderer';
+import { render } from '@testing-library/react-native';
 import App from '../App';
 
-test('renders correctly', async () => {
-  await ReactTestRenderer.act(() => {
-    ReactTestRenderer.create(<App />);
+// Mock screens
+jest.mock(
+  '../src/screens/LoanListPage/LoanListScreen',
+  () => require('../__mocks__/LoanListScreen').default,
+);
+
+jest.mock(
+  '../src/screens/LoanDetailsPage/LoanDetailScreen',
+  () => require('../__mocks__/LoanDetailScreen').default,
+);
+
+describe('App Navigation', () => {
+  it('renders LoanListScreen as initial route', () => {
+    const { getByTestId } = render(<App />);
+
+    expect(getByTestId('loan-list-screen')).toBeTruthy();
   });
 });
